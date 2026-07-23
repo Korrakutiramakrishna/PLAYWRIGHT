1f4ce201d1c61d40c3361d5fa375b8a089c6e4e7
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-23 22:17:52 +0530

    recent

--- tests/pcts.spec.js
+++ tests/pcts.spec.js
@@ -11,7 +11,22 @@ test('Windows Handling' ,async({browser})=>
    await  page.locator("li[id='menu-item-20']").click()
     await  page.waitForLoadState()
     await page.locator("//a[text()='Test Login Page']").click();
-    await  page.waitForLoadState()
-  
     
+  console.log(await expect(page.locator("#username")).toBeVisible)
+  await page.locator("#username").fill("student")
+  await page.locator("#password").fill("Password123")
+  await page.locator("//button[text()='Submit']").click();
+
+  await expect(page.locator("//h1[text()='Logged In Successfully']")).toBeVisible();
+
+const message = await page.locator("//h1[text()='Logged In Successfully']").textContent();
+
+console.log(message);
+await expect(page.locator("//strong[contains(text(),'Congratulations student. You successfully logged i')]")).toBeVisible();
+
+  const msge = await page.locator("//strong[contains(text(),'Congratulations student. You successfully logged i')]").textContent();
+  
+  console.log(msge);
+    
+
 });
